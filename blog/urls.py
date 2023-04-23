38cdac5169fa8785370d8a19e690990bfcb20e22
Hojiakbar Barotov <barotovhojiakbar.me@gmail.com>
2023-04-23 17:35:12 +0500

    Added forms: create/edit/delete for posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from .views import BlogListView, BlogDetialView
+from .views import (
+    BlogListView,
+    BlogDetialView,
+    BlogCreateView,
+    BlogUpdateView,
+    BlogDeleteView,
+)
 
 app_name = "blog"
 urlpatterns = [
+    path("post/new/", BlogCreateView.as_view(), name="post_new"),
     path("", BlogListView.as_view(), name="home"),
     path("post/<int:pk>/", BlogDetialView.as_view(), name="post_detail"),
+    path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
+    path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete"),
 ]
